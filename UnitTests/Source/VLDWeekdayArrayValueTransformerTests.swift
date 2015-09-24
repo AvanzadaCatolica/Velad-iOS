814fb8cb4ca57ead7c625ff8f79ef3033e26a30c
Renzo Crisóstomo <renzo.crisostomo@xing.com>
2015-09-24 17:31:24 +0200

    Uses Quick to describe the tests

--- UnitTests/Source/VLDWeekdayArrayValueTransformerTests.swift
+++ UnitTests/Source/VLDWeekdayArrayValueTransformerTests.swift
@@ -8,22 +8,27 @@
 
 import XCTest
 import Nimble
+import Quick
 
-class UnitTests: XCTestCase {
-    
-    override func setUp() {
-        super.setUp()
-    }
-    
-    override func tearDown() {
-        super.tearDown()
-    }
+class UnitTests: QuickSpec {
 
-    func testTransformedValue() {
-        let days = NSDateFormatter.init().weekdaySymbols;
-        let transformer = VLDWeekdayArrayValueTrasformer.init()
-        let transformedValue = transformer.transformedValue(days) as! String
-        expect(transformedValue).to(equal("7 días"))
+    override func spec() {
+        describe("VLDWeekdayArrayValueTrasformer") {
+            it("should transform correctly plural days count") {
+                let days = NSDateFormatter.init().weekdaySymbols
+                let transformer = VLDWeekdayArrayValueTrasformer.init()
+                let transformedValue = transformer.transformedValue(days) as! String
+                expect(transformedValue).to(equal("7 días"))
+            }
+
+            it("should transform correctly singular day count") {
+                let days = NSDateFormatter.init().weekdaySymbols
+                let transformer = VLDWeekdayArrayValueTrasformer.init()
+                let transformedValue = transformer.transformedValue(days.first) as! String
+                expect(transformedValue).to(equal("Sunday"))
+            }
+        }
+
     }
     
 }
